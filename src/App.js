7f58df168e2ee78451f39372e2835d9b1cc75d9d
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-01-19 18:10:14 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Button, Spinner } from "react-bootstrap";
+import GridCard from "./components/GridCard/GridCard";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Spinner animation="grow" variant="danger" />
+      <button> HTML </button>
+      <Button variant="danger">ReactBootstrap</Button>
+      <GridCard></GridCard>
     </div>
   );
 }
